18c324f16265897fff077ee905b38b8a25cd1edb
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-04-25 08:22:08 -0700

    removed

--- Real_Estate_Website/settings.py
+++ Real_Estate_Website/settings.py
@@ -157,7 +157,7 @@ AWS_SECRET_ACCESS_KEY = 'OBKJDw7XzLe2DzblFIf9Ze3OM5F2YchZM1kRmw7v'
 AWS_STORAGE_BUCKET_NAME = 'icikulwabucket'
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 [
     {
