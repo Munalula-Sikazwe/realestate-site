419226479e4b4c626ab63c043122f6b99c39d947
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-04-25 07:43:45 -0700

    added static files configurations

--- Real_Estate_Website/settings.py
+++ Real_Estate_Website/settings.py
@@ -157,3 +157,4 @@ AWS_SECRET_ACCESS_KEY = 'OBKJDw7XzLe2DzblFIf9Ze3OM5F2YchZM1kRmw7v'
 AWS_STORAGE_BUCKET_NAME = 'icikulwabucket'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
