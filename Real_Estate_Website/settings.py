06685498aa61864e0c1c3573a16d2b7f3e78107f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-04-25 08:09:35 -0700

    added cors configuration

--- Real_Estate_Website/settings.py
+++ Real_Estate_Website/settings.py
@@ -158,3 +158,19 @@ AWS_STORAGE_BUCKET_NAME = 'icikulwabucket'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+[
+    {
+        "AllowedHeaders": [
+            "*"
+        ],
+        "AllowedMethods": [
+            "POST",
+            "GET",
+            "PUT"
+        ],
+        "AllowedOrigins": [
+            "*"
+        ]
+    }
+]
